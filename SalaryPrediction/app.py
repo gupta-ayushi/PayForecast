cd98f94b0810bf1c8232633785c69d5cce6c1610
Ayushi Gupta <guptaayushi0817@gmail.com>
2024-06-19 01:45:36 +0530

    Update app.py

--- SalaryPrediction/app.py
+++ SalaryPrediction/app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from predict_page import show_predict_page
-from explore_page import show_explore_page
 
 
 page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
@@ -8,4 +7,5 @@ page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
 if page == "Predict":
     show_predict_page()
 else:
+    from explore_page import show_explore_page
     show_explore_page()
